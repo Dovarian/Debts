c19e40f0ddef543904dcc82fe8f56bf4f42fd4c5
Dovarian <vks.06.2010@mail.ru>
2025-02-21 23:24:14 +0300

    fix

--- src/app/api/users/[userID]/[creditorID]/route.ts
+++ src/app/api/users/[userID]/[creditorID]/route.ts
@@ -60,7 +60,7 @@ export async function PUT(req: NextRequest) {
     }
 }
 
-export async function GET(_: NextRequest, { params }: { params: { creditorID: number } }) {
+export async function GET(_: NextRequest, { params }: { params: Promise<{ creditorID: number }> }) {
     try {
         const creditor_id = Number((await params)?.creditorID);
 
