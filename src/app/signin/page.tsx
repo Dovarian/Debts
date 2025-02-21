ac1e19e7b6c321a750ddf5f15690346434027c6f
Dovarian <vks.06.2010@mail.ru>
2025-02-21 23:57:42 +0300

    change use client

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { RegistrationForm } from "./ui/registration-form";
 import { SignInForm } from "./ui/signIn-form";
 
--- src/app/signin/ui/registration-form.tsx
+++ src/app/signin/ui/registration-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from "react";
 import { Input } from "../../components/common/input";
 import { Button } from "./btn";
