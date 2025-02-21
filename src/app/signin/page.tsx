1fdd4a7aa17b0fb3a3974f17a303dc5f28f160d9
Dovarian <vks.06.2010@mail.ru>
2025-02-22 00:05:02 +0300

    add suspense on signin

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,18 +1,21 @@
+import { Suspense } from "react";
 import { RegistrationForm } from "./ui/registration-form";
 import { SignInForm } from "./ui/signIn-form";
 
 export default function Registration() {
     return (
-        <div className="w-1/2 h-[65vh] bg-white mx-auto mt-48 rounded shadow py-8 px-12 flex justify-between">
-            <div className="w-1/3">
-                <p className="text-4xl">Регистрация</p>
-                <RegistrationForm></RegistrationForm>
+        <Suspense fallback={"Loading..."}>
+            <div className="w-1/2 h-[65vh] bg-white mx-auto mt-48 rounded shadow py-8 px-12 flex justify-between">
+                <div className="w-1/3">
+                    <p className="text-4xl">Регистрация</p>
+                    <RegistrationForm></RegistrationForm>
+                </div>
+                <div className="h-full bg-gray-300 w-px"></div>
+                <div className="w-1/3">
+                    <p className="text-4xl">Вход</p>
+                    <SignInForm></SignInForm>
+                </div>
             </div>
-            <div className="h-full bg-gray-300 w-px"></div>
-            <div className="w-1/3">
-                <p className="text-4xl">Вход</p>
-                <SignInForm></SignInForm>
-            </div>
-        </div>
+        </Suspense>
     );
 }
